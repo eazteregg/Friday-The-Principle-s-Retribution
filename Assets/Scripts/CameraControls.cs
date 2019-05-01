32476ab2ee55ef3393f3b977edb047dc1ceaece6
eazteregg <jere@packhaus.info>
2019-05-01 11:34:43 +0200

    Implement basic movement

---
+++ Assets/Scripts/CameraControls.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour
+{
+    public Vector3 offset;
+    public GameObject player;
+    private bool detached;
+    private CharacterController characterController;
+    public float speed;
+
+    public float horizontalTurnSpeed = 2.0f;
+    public float verticalTurnSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        detached = false;
+        characterController = GetComponent<CharacterController>();
+
+    }
+
+    public bool isDetached()
+    {
+        return detached;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!detached)
+            {
+                detached = true;
+                Debug.Log("detached");
+            }
+            
+            else
+            {
+                detached = false;
+                Debug.Log("attached");
+            }
+        }
+
+    }
+
+    // LateUpdate is called once per frame after every other Update()
+    void LateUpdate()
+    {
+        if (!detached)
+        {
+            transform.position = player.transform.position + offset;
+            transform.LookAt(player.transform);
+        }
+        
+        else
+        {
+            float h = Input.GetAxis("Mouse X") * horizontalTurnSpeed;
+            float v = Input.GetAxis("Mouse Y") * verticalTurnSpeed;
+            transform.Rotate(0, h, 0, Space.World);
+
+            float horizontalMovement = Input.GetAxis("Horizontal");
+            float verticalMovement = Input.GetAxis("Vertical");
+
+            Vector3 forward = new Vector3(transform.forward.x, 0, transform.forward.z) * verticalMovement;
+            Debug.Log("Forward vector" + forward);
+            Vector3 right = transform.right * horizontalMovement;
+
+            Vector3 movementDirection = forward + right;
+            movementDirection.Normalize();
+            Debug.Log("moving");
+            characterController.Move(movementDirection * speed);
+
+        }
+
+
+
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public float horizontalInfluence = 1.0f;
+    public float jumpCooldownMAX = .3f;
+    public float maxWalljumpFallSpeed = -5.0f;
+    private float jumpCooldown = 0.0f;
+
+
+    private CharacterController characterController;
+    private Collider collider;
+
+    private Vector3 forward;
+    private Vector3 right;
+    private Vector3 moveDirection;
+
+    private Transform cameraTransform;
+    public Camera camera;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        cameraTransform = camera.transform;
+        collider = GetComponent<BoxCollider>();
+
+        forward = transform.forward;
+        right = transform.right;
+
+
+        //Debug.Log(forward);
+        //Debug.Log(right);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        moveDirection = new Vector3(0, moveDirection.y, 0);
+
+        forward = new Vector3(camera.transform.forward.x, 0, camera.transform.forward.z);
+        right = new Vector3 (camera.transform.right.x, 0, camera.transform.right.z);
+
+
+        if (characterController.isGrounded && !camera.GetComponent<CameraControls>().isDetached()) 
+        {
+            float directHoriz = Input.GetAxis("Horizontal");
+            float directVert = Input.GetAxis("Vertical");
+
+            moveDirection = (forward * directVert) + (right * directHoriz);
+            moveDirection.Normalize();
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump") && jumpCooldown <= 0.0f)
+            {
+                moveDirection.y = jumpSpeed;
+                jumpCooldown = jumpCooldownMAX;
+            }
+        }
+        if (jumpCooldown > 0.0f)
+        {
+            jumpCooldown -= 1 * Time.deltaTime;
+        }
+
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
